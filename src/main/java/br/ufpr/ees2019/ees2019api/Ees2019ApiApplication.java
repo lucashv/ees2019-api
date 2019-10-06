c2f3d06975d07a508bfa7e99d929e71ed4f3374f
Lucas Henrique Vicente <lucashv@gmail.com>
2019-10-06 17:36:04 -0300

    .

--- src/main/java/br/ufpr/ees2019/ees2019api/Ees2019ApiApplication.java
+++ src/main/java/br/ufpr/ees2019/ees2019api/Ees2019ApiApplication.java
@@ -2,8 +2,14 @@ package br.ufpr.ees2019.ees2019api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({
+	"br.ufpr.ees2019.ees2019api.repository",
+	"br.ufpr.ees2019.ees2019api.service",
+	"br.ufpr.ees2019.ees2019api.converter"
+})
 public class Ees2019ApiApplication {
 
 	public static void main(String[] args) {
