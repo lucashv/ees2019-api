7e3468bd8bb63c35ee8fc44498a2ff1f92f9f3b9
Lucas Henrique Vicente <lucashv@gmail.com>
2019-10-14 21:04:53 -0300

    Implementando Spring Security

--- src/main/java/br/ufpr/ees2019/ees2019api/config/SpringSecurityConfig.java
+++ src/main/java/br/ufpr/ees2019/ees2019api/config/SpringSecurityConfig.java
@@ -8,22 +8,32 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.ufpr.ees2019.ees2019api.security.JWTAuthenticationFilter;
+import br.ufpr.ees2019.ees2019api.security.JWTLoginFilter;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	protected void configureGlobalSecurity(AuthenticationManagerBuilder auth,
 							 			   @Qualifier("clienteService") UserDetailsService clienteService) throws Exception {
-		auth.userDetailsService(clienteService);
+		auth.inMemoryAuthentication()
+			.withUser("admin")
+			.password("{noop}admin")
+			.roles("ADMIN");
+		//auth.userDetailsService(clienteService);
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.csrf().disable()		
+			.authorizeRequests()
 			.antMatchers(HttpMethod.POST, "/login").permitAll()
 			.anyRequest().authenticated()
 			.and()
-			.csrf().disable()
+			.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 			.formLogin().disable();
 	}
 }
--- src/main/java/br/ufpr/ees2019/ees2019api/security/JWTLoginFilter.java
+++ src/main/java/br/ufpr/ees2019/ees2019api/security/JWTLoginFilter.java
@@ -1,6 +1,7 @@
 package br.ufpr.ees2019.ees2019api.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -8,31 +9,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
 
-	protected JWTLoginFilter(String url, AuthenticationManager authManager) {
-		super(new AntPathRequestMatcher(authManager))
+	public JWTLoginFilter(String url, AuthenticationManager authManager) {
+		super(new AntPathRequestMatcher(url));
 		setAuthenticationManager(authManager);
 	}
 
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		AccountCredentials credentials = 
+				new ObjectMapper().readValue(request.getInputStream(), 
+						AccountCredentials.class);
+		return getAuthenticationManager()
+				.authenticate(new UsernamePasswordAuthenticationToken(
+							credentials.getUsername(),
+							credentials.getPassword(),
+							Collections.emptyList()
+						));
 	}
 
 	@Override
 	protected void successfulAuthentication(
 			HttpServletRequest request,
-			HttpServletResponse reponse,
+			HttpServletResponse response,
 			FilterChain filterChain,
 			Authentication auth) throws IOException, ServletException {
-		
+		TokenAuthenticationService.addAuthentication(response, auth.getName());
 	}
 }
